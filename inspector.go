2232f88154ee6011f59968fca821765e1eec1b17
Jakub Jarosz <jakub@jarosz.dev>
2024-05-01 08:29:10 +0100

    Collect K8s cluster version

--- inspector.go
+++ inspector.go
@@ -1,1 +1,19 @@
 package inspector
+
+import (
+	"k8s.io/client-go/kubernetes"
+)
+
+// Client represents Inspector client.
+type Client struct {
+	K8sClient kubernetes.Interface
+}
+
+// ClusterVersion returns K8s version.
+func (c *Client) ClusterVersion() (string, error) {
+	sv, err := c.K8sClient.Discovery().ServerVersion()
+	if err != nil {
+		return "", err
+	}
+	return sv.String(), nil
+}
--- inspector_test.go
+++ inspector_test.go
@@ -1,1 +1,37 @@
 package inspector_test
+
+import (
+	"testing"
+
+	"github.com/qba73/inspector"
+	"k8s.io/apimachinery/pkg/version"
+
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
+	fakediscovery "k8s.io/client-go/discovery/fake"
+	testClient "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestInspectorCollectsK8sVersion(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(),
+	}
+	got, err := c.ClusterVersion()
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "v1.29.2"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+// newTestClientset takes K8s runtime objects and returns a k8s fake clientset.
+func newTestClientset(objects ...k8sruntime.Object) *testClient.Clientset {
+	client := testClient.NewSimpleClientset(objects...)
+	client.Discovery().(*fakediscovery.FakeDiscovery).FakedServerVersion = &version.Info{
+		GitVersion: "v1.29.2",
+	}
+	return client
+}
