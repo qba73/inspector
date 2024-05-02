16f5ff7f685245d44fef5ba7afd93c583fc17eae
Jakub Jarosz <jakub@jarosz.dev>
2024-05-02 05:56:37 +0100

    Add tests for platform id edge cases

--- inspector_test.go
+++ inspector_test.go
@@ -96,6 +96,366 @@ func TestInspectorCollectsDiagnosticData(t *testing.T) {
 	}
 }
 
+func TestInspectorCollectsPlatformNameOnAWSNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeAWS,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "aws"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnAzureNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeAzure,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "azure"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnGCPNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeGCP,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "gce"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnKindNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeKind,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "kind"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnVSphereNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeVSphere,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "vsphere"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnK3SNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeK3S,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "k3s"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnIBMCloudNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeIBMCloud,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "ibmcloud"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnIBMPowerNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeIBMPowerVS,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "ibmpowervs"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnCloudStackNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeCloudStack,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "cloudstack"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnOpenStackNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeOpenStack,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "openstack"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnDigitalOceanNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeDigitalOcean,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "digitalocean"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnEquinixMetallNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeEquinixMetal,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "equinixmetal"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorCollectsPlatformNameOnAlibabaNode(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeAlibaba,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "alicloud"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorDeterminesUnknownPlatformOnMissingPlatformIDField(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeMalformedBlankPlatformID,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "unknown"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorDeterminesUnknownPlatformOnMalformedPlatformIDField(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeMalformedPlatformID,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "//4232e3c7-d83c-d72b-758c-71d07a3d9310"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorDeterminesUnknownPlatformOnMalformedBlankPlatformIDField(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeMalformedBlankPlatformID,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "unknown"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorDeterminesUnknownPlatformOnMalformedEmptyPlatformIDField(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeMalformedEmptyPlatformID,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "unknown"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestInspectorDeterminesUnknownPlatformOnMalformedPartialPlatformIDField(t *testing.T) {
+	t.Parallel()
+
+	c := inspector.Client{
+		K8sClient: newTestClientset(
+			kubeSystemNameSpace,
+			nodeMalformedPartialPlatformID,
+		),
+	}
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "unknown"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
 // newTestClientset takes K8s runtime objects and returns a k8s fake clientset.
 func newTestClientset(objects ...k8sruntime.Object) *testClient.Clientset {
 	client := testClient.NewSimpleClientset(objects...)
@@ -156,5 +516,249 @@ var (
 			Namespace: "default",
 		},
 		Spec: corev1.NodeSpec{},
+	}
+)
+
+// Cloud providers' nodes for testing ProviderID lookups.
+var (
+	nodeAWS = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "aws:///eu-central-1a/i-088b4f07708408cc0",
+		},
+	}
+
+	nodeAzure = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "azure:///subscriptions/ba96ef31-4a42-40f5-8740-03f7e3c439eb/resourceGroups/mc_hibrid-weu_be3rr5ovr8ulf_westeurope/providers/Microsoft.Compute/virtualMachines/aks-pool1-27255451-0",
+		},
+	}
+
+	nodeGCP = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "gce://gcp-banzaidevgcp-nprd-38306/europe-north1-a/gke-vzf3z1vvleco9-pool1-7e48d363-8qz1",
+		},
+	}
+
+	nodeKind = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "kind://docker/local/local-control-plane",
+		},
+	}
+
+	nodeVSphere = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "vsphere://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeK3S = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "k3s://ip-1.2.3.4",
+		},
+	}
+
+	nodeIBMCloud = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "ibmcloud://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeIBMPowerVS = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "ibmpowervs://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeCloudStack = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "cloudstack://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeOpenStack = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "openstack://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeDigitalOcean = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "digitalocean://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeEquinixMetal = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "equinixmetal://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeAlibaba = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "alicloud://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+)
+
+// Nodes with missing or malformed PorviderID.
+var (
+	nodeMalformedPlatformID = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "//4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeMalformedPartialPlatformID = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
+	}
+
+	nodeMalformedEmptyPlatformID = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: "",
+		},
+	}
+
+	nodeMalformedBlankPlatformID = &corev1.Node{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: " ",
+		},
 	}
 )
